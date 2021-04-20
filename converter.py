47e29b19438593c256340563e6c7dc8b3043668e
Acrylic125 <acrylic125email@gmail.com>
2021-04-20 19:16:47 +0800

    Base converter (to n_10)

--- converter.py
+++ converter.py
@@ -1,35 +1,112 @@
-import layout
+import PySimpleGUI as Gui
+import numpy as np
 
 # Table to convert from char -> number
 CharNumericsTable = {
-    'A': 11,
-    'B': 12,
-    'C': 13,
-    'D': 14,
-    'E': 15,
-    'F': 16,
-    'G': 17,
-    'H': 18,
-    'I': 19,
-    'J': 20,
-    'K': 21,
-    'L': 22,
-    'M': 23,
-    'N': 24,
-    'O': 25,
-    'P': 26,
-    'Q': 27,
-    'R': 28,
-    'S': 29,
-    'T': 30,
-    'U': 31,
-    'V': 32,
-    'W': 33,
-    'X': 34,
-    'Y': 35,
-    'Z': 36
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+    'G': 16,
+    'H': 17,
+    'I': 18,
+    'J': 19,
+    'K': 20,
+    'L': 21,
+    'M': 22,
+    'N': 23,
+    'O': 24,
+    'P': 25,
+    'Q': 26,
+    'R': 27,
+    'S': 28,
+    'T': 29,
+    'U': 30,
+    'V': 31,
+    'W': 32,
+    'X': 33,
+    'Y': 34,
+    'Z': 35
 }
+NumericsCharTable = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+    16: "G",
+    17: "H",
+    18: "I",
+    19: "J",
+    20: "K",
+    21: "L",
+    22: "M",
+    23: "N",
+    24: "O",
+    25: "P",
+    26: "Q",
+    27: "R",
+    28: "S",
+    29: "T",
+    30: "U",
+    31: "V",
+    32: "W",
+    33: "X",
+    34: "Y",
+    35: "Z"
+}
+
+
+def toNum(char: str) -> int:
+    output = CharNumericsTable.get(char)
+    return output if output is not None else 0
+
+
+def validateInput(inp: Gui.Input) -> bool:
+    pass
 
 
 def validateForConversion() -> bool:
     pass
+
+
+class Converter:
+    def __init__(self,
+                 inputBase: int = 10,
+                 outputBase: int = 10,
+                 inputValue: str = "0"):
+        self.inputBase = inputBase
+        self.outputBase = outputBase
+        self.inputValue = inputValue
+
+    def toBase10(self) -> int:
+        arr = list(self.inputValue)
+        i = len(arr)
+        acc = 0
+        for c in arr:
+            i -= 1
+            acc += (toNum(c) * pow(self.inputBase, i))
+        return acc
--- main.py
+++ main.py
@@ -1,61 +1,67 @@
 import PySimpleGUI as Gui
 import layout
-
-
-def run_Convert():
-    inp = layout.e.input_Value.get()
-    layout.e.box_Output.update(inp)
-    print(inp)
-
-
-def run_Flip():
-    run_FlipBase()
-    run_FlipValues()
-
-
-def run_FlipValues():
-    inp = layout.e.input_Value
-    out = layout.e.box_Output
-    flip(inp, out)
-
-
-def run_FlipBase():
-    inp = layout.e.input_BaseInput
-    out = layout.e.input_BaseOutput
-    flip(inp, out)
-
-
-def flip(inp, out):
-    v_Inp = inp.get()
-    v_Out = out.get()
-    inp.update(v_Out)
-    out.update(v_Inp)
-
-
-def run_ButtonAction(key, values) -> bool:
-    if key == layout.KEY_BTN_CONVERT:
-        run_Convert()
-        return True
-    if key == layout.KEY_BTN_FLIP:
-        run_Flip()
-        return True
-    if key == layout.KEY_BTN_FLIP_BASE:
-        run_FlipBase()
-        return True
-    if key == layout.KEY_BTN_FLIP_INPUT:
-        run_FlipValues()
-        return True
-    return False
-
+import converter as c
 
 while True:
-    event, values = layout.window.read()
-    if event == "Exit" or event == Gui.WIN_CLOSED:
-        break
-    if run_ButtonAction(event, values):
-        continue
-    print(event)
+    base = int(input("Key in a base to convert from."))
+    val = input("Key in a value.")
+    print(c.Converter(base, inputValue=val).toBase10())
 
-
-layout.window.close()
-
+#
+# def run_Convert():
+#     inp = layout.e.input_Value.get()
+#     layout.e.box_Output.update(inp)
+#     print(inp)
+#
+#
+# def run_Flip():
+#     run_FlipBase()
+#     run_FlipValues()
+#
+#
+# def run_FlipValues():
+#     inp = layout.e.input_Value
+#     out = layout.e.box_Output
+#     flip(inp, out)
+#
+#
+# def run_FlipBase():
+#     inp = layout.e.input_BaseInput
+#     out = layout.e.input_BaseOutput
+#     flip(inp, out)
+#
+#
+# def flip(inp, out):
+#     v_Inp = inp.get()
+#     v_Out = out.get()
+#     inp.update(v_Out)
+#     out.update(v_Inp)
+#
+#
+# def run_ButtonAction(key, values) -> bool:
+#     if key == layout.KEY_BTN_CONVERT:
+#         run_Convert()
+#         return True
+#     if key == layout.KEY_BTN_FLIP:
+#         run_Flip()
+#         return True
+#     if key == layout.KEY_BTN_FLIP_BASE:
+#         run_FlipBase()
+#         return True
+#     if key == layout.KEY_BTN_FLIP_INPUT:
+#         run_FlipValues()
+#         return True
+#     return False
+#
+#
+# while True:
+#     event, values = layout.window.read()
+#     if event == "Exit" or event == Gui.WIN_CLOSED:
+#         break
+#     if run_ButtonAction(event, values):
+#         continue
+#     print(event)
+#
+#
+# layout.window.close()
+#
